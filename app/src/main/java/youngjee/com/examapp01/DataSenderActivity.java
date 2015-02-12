0839f1bfdfa664495a2d714502a0d839b8c5262c
astraltear <astraltear@gmail.com>
2015-02-12 16:17:29 +0900

    exam src add

--- app/src/main/java/youngjee/com/examapp01/DataSenderActivity.java
+++ app/src/main/java/youngjee/com/examapp01/DataSenderActivity.java
@@ -1,6 +1,9 @@
 package youngjee.com.examapp01;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,6 +33,12 @@ public class DataSenderActivity extends ActionBarActivity implements View.OnClic
         }
     }
 
+    public void goTelNo(View v) {
+        Intent contactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        contactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        startActivityForResult(contactIntent,888);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.d("EXAM",requestCode+"|"+resultCode);
@@ -39,6 +48,19 @@ public class DataSenderActivity extends ActionBarActivity implements View.OnClic
             data.getStringExtra("ADRESS");
 
             Log.d("DataSenderActivity", data.getIntExtra("AGE", 1)+"|"+data.getStringExtra("ADRESS"));
+        } else if (resultCode == RESULT_OK && requestCode == 888){
+            Uri contactUri = data.getData();
+
+            Log.d("DataSenderActivity", contactUri.getPath());
+
+            String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+            Cursor cursor = getContentResolver().query(contactUri, projection, null, null, null);
+            cursor.moveToFirst();
+
+            int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            Log.d("DataSenderActivity", "["+cursor.getString(column)+"]");
+
+
         }
     }
 }
