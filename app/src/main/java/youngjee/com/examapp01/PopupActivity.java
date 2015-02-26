0ee70292479abedf6bac1687d8235aca2fec2a21
astraltear <astraltear@gmail.com>
2015-02-26 17:28:59 +0900

    exam src add by office

--- app/src/main/java/youngjee/com/examapp01/PopupActivity.java
+++ app/src/main/java/youngjee/com/examapp01/PopupActivity.java
@@ -1,7 +1,6 @@
 package youngjee.com.examapp01;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
