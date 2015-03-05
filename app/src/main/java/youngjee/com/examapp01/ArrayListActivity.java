af8a1561dcb267fdb3a78217b0dc392a8622836e
astraltear <astraltear@gmail.com>
2015-03-05 16:52:21 +0900

    exam src add by office

--- app/src/main/java/youngjee/com/examapp01/ArrayListActivity.java
+++ app/src/main/java/youngjee/com/examapp01/ArrayListActivity.java
@@ -2,12 +2,17 @@ package youngjee.com.examapp01;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.internal.widget.AdapterViewCompat;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -29,28 +34,59 @@ public class ArrayListActivity extends ActionBarActivity {
 
         registerForContextMenu(listView);
 
+        Button btn_left = (Button) findViewById(R.id.btn_left);
+
+        registerForContextMenu(btn_left);
+
+
     }
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_array_list,menu);
+       if (v.getId() == R.id.listView){
+           MenuInflater inflater = getMenuInflater();
+           inflater.inflate(R.menu.menu_array_list, menu);
+       } else {
+           menu.setHeaderIcon(android.R.drawable.btn_star);
+           menu.setHeaderTitle("공지사항");
+           menu.add(123, 0, Menu.NONE, "APPLE");
+           menu.add(123, 1, Menu.NONE, "PEACH");
+           menu.add(123,2,Menu.NONE,"BANANA");
+
+           SubMenu subMenu = menu.addSubMenu("하우스과일>> 서버메뉴 존재");
+           subMenu.setHeaderTitle("서브메뉴 타이틀");
+           subMenu.add(456, 0, Menu.NONE, "방울토마토");
+           subMenu.add(456, 1, Menu.NONE, "딸기");
+           subMenu.add(456, 2, Menu.NONE, "호박");
+
+       }
+
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.id_menu_hi:
-                makeText(this,"HI", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.id_menu_hello:
-                makeText(this,"HELLO", LENGTH_SHORT).show();
-                break;
-            case R.id.id_menu_good:
-                makeText(this,"GOOD", LENGTH_SHORT).show();
-                break;
+
+           Log.d("item.getItemId()",item.getItemId()+"||"+item.getGroupId());
+        if (item.getGroupId() == 123){
+
+            Log.d("onContextItemSelected", "info.position[" + item.getTitle() + "]");
+        } else if(item.getGroupId() == 456){
+
+        } else {
+            switch (item.getItemId()) {
+                case R.id.id_menu_hi:
+                    makeText(this, "HI", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.id_menu_hello:
+                    makeText(this, "HELLO", LENGTH_SHORT).show();
+                    break;
+                case R.id.id_menu_good:
+                    makeText(this, "GOOD", LENGTH_SHORT).show();
+                    break;
+            }
         }
+
         return super.onContextItemSelected(item);
     }
 }
