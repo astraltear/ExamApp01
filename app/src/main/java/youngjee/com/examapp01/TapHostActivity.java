4a85ae25815bb89c35f2cac022eb5381895c3089
astraltear <astraltear@gmail.com>
2015-03-19 14:59:59 +0900

    exam src add by office

--- app/src/main/java/youngjee/com/examapp01/TapHostActivity.java
+++ app/src/main/java/youngjee/com/examapp01/TapHostActivity.java
@@ -1,13 +1,12 @@
 package youngjee.com.examapp01;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 
-public class TapHostActivity extends ActionBarActivity {
+public class TapHostActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,25 +16,23 @@ public class TapHostActivity extends ActionBarActivity {
         tabHost.setup();
 
         // Tab1 Setting
-        TabHost.TabSpec tabSpec1 = tabHost.newTabSpec("Tab1");
-        tabSpec1.setIndicator("신문, 뉴스"); // Tab Subject
-        tabSpec1.setContent(R.id.tab_view1); // Tab Content
+        TabSpec tabSpec1 = tabHost.newTabSpec("Tab1").setIndicator("신문, 뉴스").setContent(R.id.tv01);
         tabHost.addTab(tabSpec1);
 
         // Tab2 Setting
-        TabHost.TabSpec tabSpec2 = tabHost.newTabSpec("Tab2");
+        TabSpec tabSpec2 = tabHost.newTabSpec("Tab2");
         tabSpec2.setIndicator("포털, IT"); // Tab Subject
-        tabSpec2.setContent(R.id.tab_view2); // Tab Content
+        tabSpec2.setContent(R.id.tv02); // Tab Content
         tabHost.addTab(tabSpec2);
 
         // Tab3 Setting
-        TabHost.TabSpec tabSpec3 = tabHost.newTabSpec("Tab3");
+        TabSpec tabSpec3 = tabHost.newTabSpec("Tab3");
         tabSpec3.setIndicator("방송, 해외"); // Tab Subject
-        tabSpec3.setContent(R.id.tab_view3); // Tab Content
+        tabSpec3.setContent(R.id.tv03); // Tab Content
         tabHost.addTab(tabSpec3);
 
         // show First Tab Content
-        //tabHost.setCurrentTab(0);
+        tabHost.setCurrentTab(2);
     }
 
 }
