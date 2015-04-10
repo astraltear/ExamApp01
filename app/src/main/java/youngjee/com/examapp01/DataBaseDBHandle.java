1a7f3894fdafab3e317f4d0c0935175e4a99a2b8
astraltear <astraltear@gmail.com>
2015-04-10 17:32:05 +0900

    exam src add by office

---
+++ app/src/main/java/youngjee/com/examapp01/DataBaseDBHandle.java
@@ -0,0 +1,9 @@
+package youngjee.com.examapp01;
+
+
+import android.content.Context;
+
+public class DataBaseDBHandle {
+    private Context context;
+
+}
---
+++ app/src/main/java/youngjee/com/examapp01/DataBaseDBHelper.java
@@ -0,0 +1,26 @@
+package youngjee.com.examapp01;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBaseDBHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "test";
+    private static final int DB_VER = 1;
+
+    public DataBaseDBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = " create table person(id integer primary key autoincrement, name text not null) ";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
